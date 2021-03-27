04c668682a7e9d54a2d23b3312093dfbba8fe5f6
noliosahrak <wsburghardt@gmail.com>
2021-03-27 21:12:24 +0100

    sum of numbers separated by comma

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,7 @@
 class Calculator:
-    def add(self, numbers):
-        return -1;
+    def add(self, _input):
+        if not _input:
+            return 0
+        numbers = _input.split(",")
+        numbers = [int(n) for n in numbers]
+        return sum(numbers)
--- test_Calculator.py
+++ test_Calculator.py
@@ -1,10 +1,21 @@
+import pytest
 from assertpy import assert_that
 from Calculator import Calculator
 
 
 class TestCalculator:
+    @pytest.fixture
+    def calc(self):
+        self.calc = Calculator()
 
-    def test_should_return_zero_when_got_empty_string(self):
-        calc = Calculator()
-        result = calc.add("")
+    def test_should_return_zero_when_got_empty_string(self, calc):
+        result = self.calc.add("")
         assert_that(result).is_equal_to(0)
+
+    def test_should_return_number_when_given_number(self, calc):
+        result = self.calc.add("1")
+        assert_that(result).is_equal_to(1)
+
+    def test_should_add_two_numbers_separated_with_comma(self, calc):
+        result = self.calc.add("1,2")
+        assert_that(result).is_equal_to(3)
